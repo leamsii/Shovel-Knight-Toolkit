655dd7fde6d16cba1af0f80bb2e0e1a7238e6489
Liam <leamsii.e@gmail.com>
2018-10-30 22:44:38 -0400

    Add files via upload

---
+++ anb_packer.py
@@ -0,0 +1,246 @@
+from PIL import Image
+from struct import pack, unpack
+import os
+import sys
+import glob
+import json
+import time
+
+#Made by Liam :)
+
+print("#Requires Python Version 3.0 or higher.")
+
+def get_pixels(png_images):
+	print("Log: Extracting raw pixels from images...")
+	for image in png_images:
+		frame_name = os.path.basename(image)
+		frame_name = frame_name[:frame_name.find('.')]
+
+		try:
+			target_width = JSON_DATA[frame_name]['width']
+			target_height = JSON_DATA[frame_name]['height']
+		except:
+			log("Error: Could not find the image properties in the meta.json file!\n Error: Did you rename these images?")
+
+		_image = Image.open(image)
+		pixels = list(_image.getdata())
+		width,height = _image.size
+
+		if (width, height) != (target_width, target_height):
+			log("Error: The image dimensions do not match! Expected: {}, Got: {}".format(
+				(target_width, target_height), (width, height)))
+
+		pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+
+		file_name = os.path.basename(image)
+		file_name = file_name[:file_name.find('.')]
+
+		sorted_pixels = []
+		with open(file_name + '.dat', 'wb') as file:
+			for arr in pixels:
+				for tup in arr:
+					r,g,b,a = tup
+
+					r = pack('<B', r) #endianess doesn't matter
+					g = pack('<B', g) #endianess doesn't matter
+					b = pack('<B', b) #endianess doesn't matter
+					a = pack('<B', a) #endianess doesn't matter
+
+					sorted_pixels.append(r + g + b + a)
+			for pixel in sorted_pixels:
+				file.write(pixel)
+
+def set_json(directory):
+	with open(directory + '\\meta.json', 'r') as file:
+		global JSON_DATA
+		JSON_DATA = json.loads(file.read())
+
+def _sort(files):
+	arr = [_ for _ in range(len(files))]
+	for f in files:
+		_id = f[f.find('_')+1:]
+		_id = _id[:_id.find('.')]
+		
+		arr[int(_id)] = f
+	return arr
+
+def convert_to_image(data_files):
+	print("Log: Converting extracted frames to images...")
+
+	for f in data_files:
+		with open(f, 'rb') as file:
+			frame_name = f[:f.find('.')]
+			image_name = frame_name + '.png'
+			image_width = JSON_DATA[frame_name]['width']
+			image_height = JSON_DATA[frame_name]['height']
+			try:
+				image_out = Image.frombuffer('RGBA', (image_width, image_height), file.read(), 'raw', 
+					'RGBA', 0, 1)
+				image_out.save(image_name)
+			except:
+				log("Error: Something wen't wrong when converting to image {} {} {}".format(image_name,
+					image_width, image_height))
+
+def compress():
+	os.chdir(TARGET_FILE)
+	print("Log: Compressing images...")
+
+	png_images = glob.glob('*.png')
+	if len(png_images) <= 0:
+		log("Error: Could not find any .PNG images inside " + TARGET_FILE)
+	if len(png_images) != len(JSON_DATA)-1:
+		log("Error: Invalid number of frames found! Expected: {}, Got: {}".format(len(JSON_DATA)-1,
+			len(png_images)))
+
+	#Create .dat files with the raw pixel data
+	get_pixels(png_images)
+	data_files = glob.glob('*.dat')
+	for f in data_files:
+		file_name = os.path.basename(f)
+		file_name = file_name[:f.find('.')]
+		compressed_size = JSON_DATA[file_name]['compressed_size'] + 16
+
+		#Call WFLZ compressor.
+		os.system(HOME_DIRECTORY + "\\wflz_extractor\\extractor.exe " + f + ' ' + 
+			str(compressed_size))
+
+	#Now we locate the owner of the frames and swap the wflz headers.
+	print("Log: Locating the owner .ANB file...")
+	anb_file = JSON_DATA['owner']
+	if not os.path.isfile(anb_file):
+		print("Log: Could'nt find the owner .anb file for these frames...")
+		anb_location = input("Input: Would you mind telling me where its located: ").strip().replace('"', '')
+		if not os.path.isfile(anb_location):
+			clean_files()
+			log("Error: Could not find the anb file in {} or in {}".format(JSON_DATA['owner'],
+				anb_location))
+
+		anb_file = anb_location
+
+	with open(anb_file, 'rb') as file:
+		anb_data = file.read()
+
+	wflz_files = glob.glob('*.wflz')
+	wflz_files = _sort(wflz_files)
+	wflz_data = []
+	for f in wflz_files:
+		with open(f, 'rb') as file:
+			wflz_data.append(file.read())
+
+	#Now replace the wflz chunks with the edited ones
+	tmp = b''
+	for data in wflz_data:
+		wflz_offset = anb_data.find(b'WFLZ')
+		if wflz_offset == -1:
+			continue
+		padding = anb_data[:wflz_offset]
+		tmp += padding
+		tmp += data
+		anb_data = anb_data[len(data) + len(padding):]
+	tmp += anb_data
+
+	name = os.path.basename(anb_file)
+	with open('modded_' + name,'wb') as file:
+		file.write(tmp)
+
+	clean_files()
+	log("Log: Finished, check inside " + TARGET_FILE)
+
+
+def extract():
+	with open(TARGET_FILE, 'rb') as file:
+		if file.read(4) != b'YCSN':
+			log("Error: This is not a valid .anb file!")
+
+		file_data = file.read()
+
+	wflz_chunks = file_data.count(b'WFLZ')
+	if wflz_chunks <= 0:
+		log("Error: No embedded images were found in this file!")
+
+	print("Log: Creating a destination folder...")
+	if not os.path.isdir(DESTINATON_FOLDER):
+		os.system('mkdir ' + DESTINATON_FOLDER)
+	os.chdir(DESTINATON_FOLDER)
+
+	#Grab the whole data and split it for each wflz chunk
+	print("Log: Extracting {} frames...".format(wflz_chunks))
+	meta_data = {}
+	meta_data['owner'] = TARGET_FILE
+	for i in range(wflz_chunks):
+		wflz_offset = file_data.find(b'WFLZ')
+		wflz_name = 'frame_' + str(i) + '.wflz'
+
+		data_size = file_data[wflz_offset - 4: wflz_offset]
+		image_width = file_data[wflz_offset - 0x20: (wflz_offset - 0x20) + 4]
+		image_height = file_data[wflz_offset - 0x1c: (wflz_offset - 0x1c) + 4]
+		compressed_size = file_data[wflz_offset + 4: wflz_offset + 8]
+
+		meta_data['frame_' + str(i)] = { 
+			'width' : unpack('<I', image_width)[0],
+			'height' : unpack('<I', image_height)[0], 
+			'compressed_size' : unpack('<I', compressed_size)[0]
+			}
+
+		with open(wflz_name, 'wb') as wflz_file:
+			wflz_file.write(file_data[wflz_offset : wflz_offset + unpack('<I', data_size)[0]])
+
+		#Remove extracted chunk from the current data
+		file_data = file_data[wflz_offset + 1:]
+
+	#Create the meata.json file containing repacking information
+	with open('meta.json', 'w') as meta_file:
+		json.dump(meta_data, meta_file)
+
+	set_json(DESTINATON_FOLDER)
+			
+	print("Log: Decompressing frames...")
+	wflz_files = glob.glob('*.wflz')
+	for f in wflz_files:
+		os.system(HOME_DIRECTORY + "./wflz_extractor\\extractor.exe " + f)
+
+	#Now convert the decompressed wfzl files to png images
+	convert_to_image(glob.glob('*.dat'))
+	clean_files()
+	log("Log: Finished, check inside " + DESTINATON_FOLDER)
+
+def clean_files():
+	print("Log: Cleaning up...")
+	dat_files = glob.glob('*.dat')
+	wflz_files = glob.glob('*.wflz')
+	for file in zip(dat_files, wflz_files):
+		os.system("del " + file[0])
+		os.system("del " + file[1])
+
+def log(msg):
+	print(msg)
+	print("Exiting in 5 seconds...")
+	time.sleep(5)
+	sys.exit(-1)
+
+#Validate user input
+if len(sys.argv) != 2:
+	log("Error: Please specify a target .anb file or a .dat file")
+
+#Define global variables
+HOME_DIRECTORY = os.path.dirname(sys.argv[0])
+TARGET_FILE = sys.argv[1]
+TARGET_NAME = os.path.basename(TARGET_FILE)[:os.path.basename(TARGET_FILE).find('.')]
+TARGET_FORMAT = TARGET_FILE[TARGET_FILE.find('.'):]
+TARGET_DIRECTORY = os.path.dirname(TARGET_FILE)
+DESTINATON_FOLDER = os.path.dirname(TARGET_FILE) + '\\' + TARGET_NAME
+JSON_DATA = None
+
+#Everthing went well start program
+if os.path.isdir(TARGET_FILE):
+	if not os.path.isfile(TARGET_FILE + './meta.json'):
+		log("Error: Could not find the generated meta.json file in " + TARGET_FILE)
+
+	set_json(TARGET_FILE)
+	compress()
+else:
+	if not os.path.isfile(TARGET_FILE):
+		log("Error: The target file '{}' was not found!".format(TARGET_FILE))
+	extract()
+
+log("Log: Program finished.")
---
+++ pak_packer.py
@@ -0,0 +1,198 @@
+import struct
+import os
+import sys
+import time
+import glob
+
+#Made by Liam :)
+
+print("#Requires Python Version 3.0 or higher.")
+def sort_files(files):
+	sorted_files = []
+	subfiles = []
+
+	#Remove directories
+	for f in files:
+		if os.path.isdir(f) == False:
+			subfiles.append(f)
+
+	#Sort by order id
+	for f in subfiles:
+		with open(f, 'rb') as file:
+			metadata = file.read()
+			metadata_offset = metadata.find(b'LIAM')
+			if metadata_offset == -1:
+				print("Warning: The META header in {} was not found!".format(f))
+				return subfiles
+
+			metadata = metadata[metadata_offset + 4 : ]
+			order_id = struct.unpack('<I', metadata[0x20 : ])[0]
+			sorted_files.append((order_id, f))
+
+	sorted_files.sort()
+	subfiles = []
+	for k,v in enumerate(sorted_files):
+		subfiles.append(v[1])
+
+	return subfiles
+
+
+def create_name_chunk(names):
+	data = b''
+	for name in names:
+		name = name.replace('\\', '/')
+		data += name.encode() + bytes(2)
+	return data
+
+def get_names(file, offset):
+	file.seek(offset)
+	names = [v for v in file.read().split(b'\x00') if v]
+	return names
+
+def log(msg):
+	print(msg)
+	print("Exiting in 5 seconds...")
+	time.sleep(5)
+	sys.exit(-1)
+
+def pack(folder):
+	os.chdir(folder)
+	files = glob.glob('**', recursive=True)
+	if len(files) <= 0:
+		log("Error: No files were found in " + folder)
+
+	print("Log: Repacking files...")
+
+	subfiles = sort_files(files)
+	file_datas = []
+	file_size = 0
+
+	#Seperate the files and get raw data
+	print("Log: Collecting raw data...")
+	for f in subfiles:
+		with open(f, 'rb') as file:
+			v = file.read()
+			file_datas.append(v)
+			file_size += len(v) - 8 # -8 is for the metadata header and order id
+
+	print("Log: Writing header...")
+	#Files collected now begin writing header
+	data = bytes(4) #First empty 4 bytes
+	data += struct.pack('<I', len(subfiles)) #number of files
+	data += struct.pack('<I', 0x18) #data offset
+	data += bytes(4) #padding
+	name_offset = struct.pack('<I', 0x18 + (8 * len(subfiles))  + file_size)
+	data += bytes(name_offset)
+	data += bytes(4) #padding
+
+	print("Log: Writing offsets...")
+	#Begin adding the data offsets
+	max_length = 0x20 + (8 * (len(subfiles) - 1))
+	last_length = 0
+	for k,v in enumerate(file_datas):
+		offset = max_length + last_length
+		data += struct.pack('<I', offset) + bytes(4)
+		last_length += (len(v) - 8)
+
+
+	print("Log: Writing raw data...")
+	#Add raw data
+	for k,v in enumerate(file_datas):
+		metadata_offset = v.find(b'LIAM')
+		if metadata_offset != -1:
+			metadata = v[metadata_offset + 4 : (metadata_offset + 4) + 0x20] #Thus removing the order id
+			v = v[ : metadata_offset]
+			data += metadata + v
+		else:
+			data += v
+
+	print("Log: Writing name offsets...")
+	#Writes names
+	max_length = 8 * len(subfiles)
+	start_offset = len(data) + max_length
+	last_length = 0
+	for k,v in enumerate(subfiles):
+		v = v.replace('\\', '/')
+		start_offset += last_length
+		data += struct.pack('<I', start_offset) + bytes(4)
+		last_length = len(v) + 2 # +2 for the padding
+
+	names = create_name_chunk(subfiles)
+	data += names
+
+	with open(os.path.basename(folder) + '.pak', 'wb') as file:
+		file.write(data)
+
+	print("Log: Finished, check inside " + folder)
+
+
+def unpack(f):
+	#Get header values
+	with open(f, 'rb') as file:
+		if len(file.read()) <= 50:
+			log("Error: The PAK file is too small " + f)
+
+		file.seek(4)
+		num_files = struct.unpack('<I', file.read(4))[0]
+		data_offset = struct.unpack('<I', file.read(4))[0]
+		file.seek(0x10)
+		name_offset = struct.unpack('<I', file.read(4))[0]
+		file.seek(name_offset)
+		name_offset = struct.unpack('<I', file.read(4))[0]
+		names = get_names(file, name_offset)
+
+		#Get data offsets
+		data_offsets = []
+		data_sets = []
+		file.seek(data_offset)
+		for i in range(num_files):
+			data_offsets.append(struct.unpack('<I', file.read(4))[0] + 0x20)
+			file.read(4) #Skip 4
+
+		#Get data chunk and metadata for repacking.
+		for k, offset in enumerate(data_offsets):
+			file.seek(offset - 0x20)
+			metadata = b'LIAM' + file.read(0x20) + struct.pack('<I', k)
+			file.seek(offset - 0x20)
+			data_size = struct.unpack('<I', file.read(4))[0]
+			file.seek(offset)
+			data = file.read(data_size)
+			data_sets.append(data + metadata)
+
+		if len(data_sets) != num_files or len(names) != num_files:
+			log("Error: Some values did not match: Files: {}, Data Sets: {}, Names: {}".format(num_files, len(data_sets), len(names)))
+
+		pak_file = os.path.basename(f)
+		pak_file = pak_file[:pak_file.find('.')]
+		os.chdir(os.path.dirname(f))
+		os.system('mkdir ' + pak_file)
+
+		for k, name in enumerate(names):
+			directory = os.path.dirname(name.decode()).replace('/', '\\')
+			file_name = os.path.basename(name.decode())
+			path = pak_file + '\\' + directory
+
+			print("Log: Extracting {} to {}".format(file_name, path))
+			if os.path.isdir(path) == False:
+				os.system('mkdir ' + path)
+
+			with open(path + '\\' + file_name, 'wb') as file:
+				file.write(data_sets[k])
+
+args = sys.argv
+if len(args) != 2:
+	print("Info: ###Commands###")
+	print("Info: sk_packer.py target.pak, to unpack a file")
+	print("Info: sk_packer.py target_folder, to pack a folder")
+	log("")
+
+target = args[1]
+if os.path.isfile(target):
+	unpack(target)
+elif os.path.isdir(target):
+	pack(target)
+else:
+	log("Error: File or folder was not found " + target)
+
+print("Log: Program finished.")
+log("")
