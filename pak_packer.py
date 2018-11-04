730ab09eca24f999c85e26820ab1de3123cdf47c
liam <leamsii.e@gmail.com>
2018-11-04 17:38:33 -0500

    Update pak_packer.py
    Increased repacking speed and added file change detection feature

--- pak_packer.py
+++ pak_packer.py
@@ -1,9 +1,9 @@
 import os
 import sys
 import struct
+import zlib
 from time import sleep
 from glob import glob
-from zlib import crc32
 
 def log(msg):
 	print(msg)
@@ -20,149 +20,151 @@ def get_extension(filename):
 		log("Error: {} has no extension!".format(filename))
 	return filename[extension_offset:]
 
-def create_metafile(directory, names, sub_data, pak_data, pak_name):
-	#Name then checksum then at the end the pak data
+def create_metafile(directory, data_sets, pak_data):
+	#checksum, raw data
 	with open(directory + '\\meta.dat', 'wb') as file:
-		file.write(struct.pack('<H', len(names)))
-		for k, v in enumerate(names):
-			file.write(struct.pack('<H', len(v)) + v)
+		file.write(struct.pack('<H', len(data_sets))) #How many checksums to read
+		for data in data_sets:
+			file.write(struct.pack('<I', zlib.crc32(data))) #Create a checksum for the current data set
 
-			#Generate the checksum
-			file.write(struct.pack('<I', crc32(sub_data[k])))
+		#Now the original data
+		file.write(zlib.compress(pak_data))
 
-			#include original data
-			file.write(struct.pack('<I', len(sub_data[k])))
-			file.write(sub_data[k])
+def get_filenames(file):
+	with open(file, 'rb') as file:
+		file.seek(0x10)
+		name_offset = struct.unpack('<I', file.read(4))[0]
+		file.seek(name_offset)
+		name_offset = struct.unpack('<I', file.read(4))[0]
+		file.seek(name_offset)
+		names = [v for v in file.read().split(b'\x00') if v]
+	return names
 
-		#Write the original .pak data
-		file.write(struct.pack('<H', len(pak_name)))
-		file.write(pak_name.encode('utf-8'))
-		file.write(pak_data)
+
+def get_datasets(file):
+	data_sets = []
+	with open(file, 'rb') as file:
+		file.seek(4)
+		num_files = struct.unpack('<I', file.read(4))[0]
+		data_offset = struct.unpack('<I', file.read(4))[0]
+
+		#Get data offsets
+		data_offsets = []
+		file.seek(data_offset)
+
+		for _ in range(num_files):
+			data_offsets.append(struct.unpack('<I', file.read(4))[0] + 0x20)
+			file.read(4) #Skip 4 bytes
+
+		#Get data chunks 
+		for k, offset in enumerate(data_offsets):
+			file.seek(offset - 0x20)
+			data_size = struct.unpack('<I', file.read(4))[0]
+			file.seek(offset)
+			data = file.read(data_size)
+			data_sets.append(data)
+
+	return data_sets
 
 def pack(f):
 	os.chdir(f)
 	if not os.path.isfile('meta.dat'):
 		log("Error: Could not find the meta.dat file in " + f)
 
-	_pack = struct.pack
-	_unpack = struct.unpack
-
-	#Collect file information
-	file_names = {}
-	pak_data = b''
-	pak_name = b''
 	with open('meta.dat', 'rb') as file:
-		num_files = _unpack('<H', file.read(2))[0]
-		for _ in range(num_files):
-			name_size = _unpack('<H', file.read(2))[0]
-			name = file.read(name_size).decode('utf-8')
-			checksum = _unpack('<I', file.read(4))[0]
+		num_checksums = struct.unpack('<H', file.read(2))[0]
+		checksums = []
+		pak_data = b''
+		for _ in range(num_checksums):
+			checksums.append(struct.unpack('<I', file.read(4))[0])
+		pak_data = zlib.decompress(file.read())
 
-			data_size = _unpack('<I', file.read(4))[0]
-			data = file.read(data_size)
-
-			file_names[name] = {'checksum' : checksum, 'data' : data}
-
-		pak_name_size = _unpack('<H', file.read(2))[0]
-		pak_name = file.read(pak_name_size).decode('utf-8')
-		pak_data = file.read()
-
-	#Locate any edited files or missing files
-	changed_files = {}
-	missing_files = []
-	for k, name in enumerate(file_names):
-		checksum = file_names[name]['checksum']
-		try:
-			with open(name, 'rb') as file:
-				if crc32(file.read()) != checksum: #We found an edited file lets swap the values
-					file.seek(0)
-					changed_files[name] = {'changed_data' : file.read(), 
-					'original_data' : file_names[name]['data']}
-		except:
-			missing_files.append(name)
-
-	if len(missing_files) != 0:
-		print("Error: Missing required files...")
-		for file in missing_files:
-			print("Missing: " + file)
-		log("")
-
-	#Now swap any edited files
-	print("Log: Repacking files...")
-	for k, file in enumerate(changed_files):
-		pak_data = pak_data.replace(changed_files[file]['original_data'], changed_files[file]['changed_data'])
-
-	with open(pak_name, 'wb') as file:
+	#We have the checksums and the data collected
+	with open('tmp.bin', 'wb') as file:
 		file.write(pak_data)
 
-	log("Log: Finished, check inside " + f)
+	file_names = get_filenames('tmp.bin')
+	data_sets = get_datasets('tmp.bin')
+	os.system('del tmp.bin')
 
+	missing_files = False
+	edited_files = {}
+	for k, v in enumerate(file_names):
+		v = v.decode('utf-8')
+		try:
+			with open(v, 'rb') as file:
+				if missing_files:
+					return
+				new_checksum = zlib.crc32(file.read())
+				old_checksum = checksums[k]
+				if new_checksum != old_checksum:
+					file.seek(0)
+					new_data = file.read()
+					old_data = data_sets[k]
+					data_sets[k] = new_data
+
+					edited_files[v] = {'new_data' : new_data, 'old_data' : old_data}
+		except:
+			missing_files = True
+			print("Error: Missing file " + v)
+			#Continue running to see if any more missing files
+			
+	if missing_files:
+		log("")
+
+	if len(edited_files) > 0:
+		print("Log: Detected {} edited files...".format(len(edited_files)))
+
+		for ff in edited_files:
+			old_data = edited_files[ff]['old_data']
+			new_data = edited_files[ff]['new_data']
+			pak_data = pak_data.replace(old_data, new_data)
+
+		#Now overwright the metafile
+		create_metafile(os.getcwd(), data_sets, pak_data)
+	else:
+
+		print("Log: No change in the files was found...")
+
+	with open(os.path.basename(f) + '.pak', 'wb') as file:
+		file.write(pak_data)
+
+	log("Log: Finished, check inside " + os.path.basename(f))
 
 
 def unpack(f):
 	if os.stat(f).st_size <= 50:
 		log("Error: The PAK file is too small " + f)
 
-	_pack = struct.pack
-	_unpack = struct.unpack
+	file_names = get_filenames(f)
+	data_sets = get_datasets(f)
 
-	#Get header values
+	print("Log: Extracting {} files...".format(len(file_names)))
+
+	os.chdir(os.path.dirname(f))
+	
+	#Create the directory
+	pak_filename = os.path.basename(f)
+	pak_filename = pak_filename[:pak_filename.find('.')]
+	if not os.path.isdir(pak_filename):
+		os.system('mkdir ' + pak_filename)
+
+	#The meta file will just be the compressed .pak original file for refrencing
 	with open(f, 'rb') as file:
-		fseek = file.seek
-		fread = file.read
+		pak_data = file.read()
 
-		pak_data = fread() #Used for repacking
-		fseek(0)
+	create_metafile(pak_filename, data_sets, pak_data)
 
-		fseek(4)
-		num_files = _unpack('<I', fread(4))[0]
-		data_offset = _unpack('<I', fread(4))[0]
-		fseek(0x10)
-		name_offset = _unpack('<I', fread(4))[0]
-		fseek(name_offset)
-		name_offset = _unpack('<I', fread(4))[0]
-		fseek(name_offset)
-		names = [v for v in fread().split(b'\x00') if v]
+	#Create the directories and files needed
+	for k, name in enumerate(file_names):
+		directory = os.path.dirname(name.decode()).replace('/', '\\')
+		file_name = os.path.basename(name.decode())
+		path = pak_filename + '\\' + directory
+		if not os.path.isdir(path):
+			os.system('mkdir ' + path)
 
-		#Get data offsets
-		data_offsets = []
-		fseek(data_offset)
-		for _ in range(num_files):
-			data_offsets.append(_unpack('<I', fread(4))[0] + 0x20)
-			fread(4) #Skip 4 bytes
-
-		data_sets = []
-		#Get data chunks 
-		for k, offset in enumerate(data_offsets):
-			fseek(offset - 0x20)
-			data_size = _unpack('<I', fread(4))[0]
-			fseek(offset)
-			data = fread(data_size)
-			data_sets.append(data)
-
-		if len(data_sets) != num_files or len(names) != num_files:
-			log("Error: Some values did not match: Files: {}, Data Sets: {}, Names: {}".format(num_files, 
-				len(data_sets), len(names)))
-
-		#We have the required values now create the metafile for repacking
-		os.chdir(os.path.dirname(f))
-		print("Log: Extracting {} files...".format(num_files))
-
-		pak_file = os.path.basename(f)
-		pak_file = pak_file[:pak_file.find('.')]
-		os.system('mkdir ' + pak_file)
-
-		create_metafile(pak_file, names, data_sets, pak_data, os.path.basename(f))
-
-		for k, name in enumerate(names):
-			directory = os.path.dirname(name.decode()).replace('/', '\\')
-			file_name = os.path.basename(name.decode())
-			path = pak_file + '\\' + directory
-			if not os.path.isdir(path):
-				os.system('mkdir ' + path)
-
-			with open(path + '\\' + file_name, 'wb') as file:
-				file.write(data_sets[k])
+		with open(path + '\\' + file_name, 'wb') as file:
+			file.write(data_sets[k])
 
 
 args = sys.argv
